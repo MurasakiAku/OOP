11239331f40eff5d9da69f84bf7c8bf78af83fa2
MurasakiAku <Nastya-13-14@yandex.ru>
2024-11-13 21:06:08 +0700

    new
    new

---
+++ Mendoukusai/Mendoukusai/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Mendoukusai.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
