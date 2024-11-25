ddd6d8e47741ef5769fd82dc6ad3f902abb393cb
MurasakiAku <Nastya-13-14@yandex.ru>
2024-11-25 22:25:32 +0700

    update
    update

---
+++ Mendoukusai/Mendoukusai/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Mendoukusai.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mendoukusai.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options ): base (options)
+        {
+                
+        }
+        public DbSet<Category> Category {get;set;}
+    }
+}
---
+++ Mendoukusai/Mendoukusai/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mendoukusai.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; } 
+        public string Name { get; set; }    
+        public int DisplayOrder { get; set; }
+    }
+}
